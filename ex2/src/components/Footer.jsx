4df03d1de7e0cf0045d6565b5d54f4bfdd84d1e1
MagalGabriel <gabrielmaaga@gmail.com>
2023-08-31 20:48:56 -0300

    adicionei um gif sem fundo no footer

--- ex2/src/components/Footer.jsx
+++ ex2/src/components/Footer.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import './Footer.css'
+import animation from '../assets/gifs/CrimsonRose-rotation.gif'
+
 const Footer = () => {
   return (
     <footer>
         <p>Todos os direitos reservados - 2023 .</p>  
         <p>. Desenvolvido por Gabriel Magalhães</p>
+
+        <img src={animation} className='animation' alt="Rosa carmesim rodando" />
     </footer>
   )
 }
