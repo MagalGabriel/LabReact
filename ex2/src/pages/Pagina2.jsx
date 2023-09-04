58f1f1e34f91488f56766d0445ec318e6e20c99e
MagalGabriel <gabrielmaaga@gmail.com>
2023-09-04 18:11:29 -0300

    Adicionei a imagem da Beidou na página 2

--- ex2/src/pages/Pagina2.jsx
+++ ex2/src/pages/Pagina2.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import BeidouImg from "../assets/images/Beidou02.jpg"
 
 const Pagina2 = () => {
   return (
@@ -6,6 +7,7 @@ const Pagina2 = () => {
         <section>
             <h2>Página 2</h2>
             <p>Esta é a página dois</p>
+            <img src={BeidouImg} alt='Personagem Beidou de Genshin Impact, segurando uma máscara kitsune perto do rosto' />
         </section>
     </main>
   )
